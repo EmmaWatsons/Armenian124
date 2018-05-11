673086a40ffab2ab27c4b0564407770b30b0f67e
EmmaWatsons <andrewding@berkeley.edu>
2018-05-11 09:19:20 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,281 @@
+$(document).ready(function() {
+
+	$("#Kekel1").click(function(){
+   window.location.href="kekel.html";
+})
+
+	$("#Epemia1").click(function(){
+   window.location.href="epemia.html";
+})
+
+	$("#Pepo1").click(function(){
+   window.location.href="pepo.html";
+})
+
+	$("#Agafya1").click(function(){
+   window.location.href="agafya.html";
+})
+
+	$("#Pod1").click(function(){
+   window.location.href="pod.html";
+})
+
+	$("#Hanumig1").click(function(){
+   window.location.href="hanumig.html";
+})
+
+	$("#Arousiag1").click(function(){
+   window.location.href="arousiag.html"
+})
+
+	$("#Arshag1").click(function(){
+   window.location.href="arshag.html";
+})
+
+	$("#kekelb1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#epemiab1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#pepob1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#arousiagb1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#hanumigb1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#arshagb1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#agafyab1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#podb1").click(function(){
+   window.location.href="index.html";
+})
+
+	$("#kekelmessages").click(function(){
+   window.location.href="kekel2.html";
+})
+
+	$("#epemiamessages").click(function(){
+   window.location.href="epemia2.html";
+})
+
+	$("#pepomessages").click(function(){
+   window.location.href="pepo2.html";
+})
+
+	$("#arousiagmessages").click(function(){
+   window.location.href="arousiag2.html";
+})
+
+	$("#hanumigmessages").click(function(){
+   window.location.href="hanumig2.html";
+})
+
+	$("#arshagmessages").click(function(){
+   window.location.href="arshag2.html";
+})
+
+	$("#agafyamessages").click(function(){
+   window.location.href="agafya2.html";
+})
+
+	$("#podmessages").click(function(){
+   window.location.href="pod2.html";
+})
+
+	$("#kekelb2").click(function(){
+   window.location.href="kekel.html";
+})
+
+	$("#epemiab2").click(function(){
+   window.location.href="epemia.html";
+})
+
+	$("#pepob2").click(function(){
+   window.location.href="pepo.html";
+})
+
+	$("#arousiagb2").click(function(){
+   window.location.href="arousiag.html";
+})
+
+	$("#hanumigb2").click(function(){
+   window.location.href="hanumig.html";
+})
+
+	$("#arshagb2").click(function(){
+   window.location.href="arshag.html";
+})
+
+	$("#agafyab2").click(function(){
+   window.location.href="agafya.html";
+})
+
+	$("#podb2").click(function(){
+   window.location.href="pod.html";
+})
+
+	$("#kekelarshag").click(function(){
+   window.location.href="1.html";
+})
+
+	$("#1b").click(function(){
+   window.location.href="kekel2.html";
+})
+
+	$("#kekelpod").click(function(){
+   window.location.href="2.html";
+})
+
+	$("#2b").click(function(){
+   window.location.href="kekel2.html";
+})
+
+	$("#epemiaarshag").click(function(){
+   window.location.href="3.html";
+})
+
+	$("#3b").click(function(){
+   window.location.href="epemia2.html";
+})
+
+	$("#pepoarousiag").click(function(){
+   window.location.href="4.html";
+})
+
+	$("#4b").click(function(){
+   window.location.href="pepo2.html";
+})
+
+	$("#pepoagafya").click(function(){
+   window.location.href="5.html";
+})
+
+	$("#5b").click(function(){
+   window.location.href="pepo2.html";
+})
+
+	$("#pepohanumig").click(function(){
+   window.location.href="6.html";
+})
+
+	$("#6b").click(function(){
+   window.location.href="pepo2.html";
+})
+
+	$("#agafyapepo").click(function(){
+   window.location.href="7.html";
+})
+
+	$("#7b").click(function(){
+   window.location.href="agafya2.html";
+})
+
+	$("#agafyaarshag").click(function(){
+   window.location.href="8.html";
+})
+
+	$("#8b").click(function(){
+   window.location.href="agafya2.html";
+})
+
+	$("#podkekel").click(function(){
+   window.location.href="9.html";
+})
+
+	$("#9b").click(function(){
+   window.location.href="pod2.html";
+})
+
+	$("#podhanumig").click(function(){
+   window.location.href="10.html";
+})
+
+	$("#10b").click(function(){
+   window.location.href="pod2.html";
+})
+
+	$("#podarousiag").click(function(){
+   window.location.href="11.html";
+})
+
+	$("#11b").click(function(){
+   window.location.href="pod2.html";
+})
+
+	$("#hanumigpepo").click(function(){
+   window.location.href="12.html";
+})
+
+	$("#12b").click(function(){
+   window.location.href="hanumig2.html";
+})
+
+	$("#hanumigpod").click(function(){
+   window.location.href="13.html";
+})
+
+	$("#13b").click(function(){
+   window.location.href="hanumig2.html";
+})
+
+	$("#arousiagpepo").click(function(){
+   window.location.href="14.html";
+})
+
+	$("#14b").click(function(){
+   window.location.href="arousiag2.html";
+})
+
+	$("#arousiagpod").click(function(){
+   window.location.href="15.html";
+})
+
+	$("#15b").click(function(){
+   window.location.href="arousiag2.html";
+})
+
+	$("#arshagkekel").click(function(){
+   window.location.href="16.html";
+})
+
+	$("#16b").click(function(){
+   window.location.href="arshag2.html";
+})
+
+	$("#arshagepemia").click(function(){
+   window.location.href="17.html";
+})
+
+	$("#17b").click(function(){
+   window.location.href="arshag2.html";
+})
+
+	$("#arshagagafya").click(function(){
+   window.location.href="18.html";
+})
+
+	$("#18b").click(function(){
+   window.location.href="arshag2.html";
+})
+
+	$("#tinder_logo").click(function(){
+   window.location.href="index.html";
+})
+
+
+
+});
